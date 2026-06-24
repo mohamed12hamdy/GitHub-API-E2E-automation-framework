7e15441a2a06ed7d0f8853a6fa46a7d8e252edc0
MohamedHamdy <midohamdi487@gmail.com>
2026-06-24 16:33:19 +0300

    new secret

--- github-api-automation/src/main/java/config/EnvironmentVariables.java
+++ github-api-automation/src/main/java/config/EnvironmentVariables.java
@@ -2,5 +2,5 @@ package config;
 
 public class EnvironmentVariables {
     public static final String GITHUB_TOKEN =
-            System.getenv("GITHUB_TOKEN");
+            System.getenv("TOKEN");
 }
